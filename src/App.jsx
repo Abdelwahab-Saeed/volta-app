cc5d25b7a0ffdd4471a6ca8f5a41a6e2cab34345
Malik Hussein <malikhussein229@gmail.com>
2025-12-22 16:30:27 +0200

    feat: implement footer component with newsletter subscription and contact information

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Routes } from 'react-router-dom'
-import Header from './components/layout/Header'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { Routes } from 'react-router-dom';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
 
 function App() {
-
   return (
     <>
       <Header />
-      <Routes>
-
-      </Routes>
+      <Routes></Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
