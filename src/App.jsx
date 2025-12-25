e4b2d99836865d5ba9dd0225a7cc9498e91febe7
Abdelwahab Saeed Younes <abdelwahabsaeed415@gmail.com>
2025-12-25 15:30:27 +0200

    Add Products page and integrate routing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,17 @@ import Register from './pages/Register';
 import Footer from './components/layout/Footer';
 import Login from './pages/Login';
 import Home from './pages/Home';
+import Products from './pages/Products';
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={ <Home /> } />
+        <Route path="/register" element={ <Register /> } />
+        <Route path="/login" element={ <Login />} />
+        <Route path='/products' element={ <Products /> } />
       </Routes>
       <Footer />
     </>
