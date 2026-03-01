1c13919376d3cbb9b246c74210018fcd22712044
Abdelwahab Saeed Younes <abdelwahabsaeed415@gmail.com>
2026-03-02 01:03:06 +0200

    feat: Implement a sub-footer displaying copyright and developer information.

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -167,6 +167,15 @@ export default function Footer() {
           </div>
         </div>
       </div>
+
+      {/* Sub-footer */}
+      <div className="bg-primary border-t border-white/10 py-4">
+        <div className="container mx-auto px-4 text-center">
+          <p className="text-sm opacity-70">
+            Developed and Maintained by Falak Innovation © All Rights Reserved - Volta
+          </p>
+        </div>
+      </div>
     </footer>
   );
 }
